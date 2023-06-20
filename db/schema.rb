5f5b5f8d96d9882ad6c54bbb44fc96cc9d0c0007
KelvinMain <chunheichang@gmail.com>
2023-06-20 15:52:23 +0800

    cat

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_064350) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_075200) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
