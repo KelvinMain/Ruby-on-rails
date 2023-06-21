cae048597759122fbd876d79a15b79595d488847
KelvinMain <chunheichang@gmail.com>
2023-06-21 22:59:44 +0800

    DONE

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'categories/edit', to: 'categories#edit'
   get '/search',to: "books#search"
   devise_for :users
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :books
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
